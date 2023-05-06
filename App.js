94a17e5a0748d391edf1edc868d1836362525b68
kozakevich13 <hanibalsmitti@gmail.com>
2023-05-06 14:31:52 +0300

    add DetailsScreen

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import ShearchScreen from './components/ShearchScreen';
-import ShowDetailsScreen from './components/ShowDetailsScreen';
+import DetailsScreen from './components/DetailsScreen';
 
 
 const Stack = createStackNavigator();
@@ -12,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Shearch" component={ShearchScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ components/DetailsScreen.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Image, Linking } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Button } from 'react-native-elements';
+import axios from 'axios';
+
+
+export default function DetailsScreen({ route }) {
+  const { id, name } = route.params;
+  const [show, setShow] = useState(null);
+
+
+  useEffect(() => {
+    const fetchShowDetails = async () => {
+      try {
+        const response = await axios.get(`https://api.tvmaze.com/shows/${id}`);
+        const data = response.data;
+        setShow(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+
+    fetchShowDetails();
+  }, [id]);
+
+
+  const openShowLink = () => {
+    if (show?.officialSite) {
+      Linking.openURL(show.officialSite);
+    }
+  };
+
+
+  if (!show) {
+    return null;
+  }
+
+
+  const { image, genres, rating, status, schedule, summary } = show;
+
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image style={styles.image} source={{ uri: image?.original }} />
+      <View style={styles.details}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.genres}>{genres.join(', ')}</Text>
+        <Text style={styles.rating}>Rating: {rating.average}</Text>
+        {show.officialSite && (
+          <Button
+            title="Official Site"
+            onPress={openShowLink}
+            containerStyle={styles.linkButtonContainer}
+          />
+        )}
+        <Text style={styles.status}>Status: {status}</Text>
+        <Text style={styles.schedule}>
+          Schedule: {schedule.days.join(', ')} at {schedule.time}
+        </Text>
+        <Text style={styles.summary}>{summary.replace(/<[^>]+>/g, '')}</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 3 / 2,
+    resizeMode: 'cover',
+  },
+  details: {
+    flex: 1,
+    alignSelf: 'stretch',
+    padding: 20,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  genres: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    marginBottom: 10,
+  },
+  rating: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  linkButtonContainer: {
+    alignSelf: 'flex-start',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  status: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  schedule: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  summary: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
